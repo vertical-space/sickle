b4f3fbcf703ca92295dd1d349586acc7ddd6cc72
vertical-space <chris.cowled@csiro.au>
2013-10-03 15:16:29 +1000

    minor changes

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-qual = 57	# variable (Phred score)
+from sys import argv
 
+
+
+infile = argv[1]	# input file. Must be fastq format, preferably .fastq
+outfile = argv[1].rstrip('.fastq')+'_trimmed.fastq'
+
+qual = argv[2]	# minimum average quality across window, (Phred score), e.g. 57
 qual_min = qual + 33
-len_min = 30
-win = int(10)
 
-infile = 'example.fastq'
-outfile = 'example_trimmed.fastq'
+len_min = argv[3]	# minimum length threshold after trimming, e.g. 30
+win = int(argv[4])	# window size, e.g. 10
 
 a = open(infile, 'r').read().strip().split('@')
 z = open(outfile, 'w')
@@ -14,27 +18,13 @@ discard = 0
 trimmed = 0
 intact = 0
 
-def trim1(Q):
-    global qual_min, len_min
-    out = ''
-    for chr in Q:
-        if ord(chr) < qual_min:
-            break
-        out += chr
-    if len(out) > len_min:
-        #print out
-        return len(Q)
-    else:
-        return None
-
-def trim2(Q):
+def trim(Q):
     global qual_min, len_min, win
     endpoint = len(Q)-win
     out = ''
     qualscores = [ord(chr) for chr in Q]
     x = 0
     while True:
-        #print x,endpoint
         average = sum(qualscores[x:x+win])/float(win)
         if average < qual_min or x > endpoint:
             break
@@ -44,12 +34,13 @@ def trim2(Q):
     else:
         return None
 
+
 for line in a[1:]:
     b = line.strip()
     c = b.split('\n')
     if len(c) == 4:
         Q = c[3]
-        d = trim2(Q)
+        d = trim(Q)
         if d != None:
             out = '\n'.join(['@'+c[0],c[1][:d+1],c[2],c[3][:d+1]])+'\n'
             if d < len(Q):
@@ -63,3 +54,4 @@ for line in a[1:]:
 print 'intact reads:', intact
 print 'trimmed reads:', trimmed
 print 'discarded reads:', discard
+
