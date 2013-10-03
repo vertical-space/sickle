69409f2cbed5610d2307903e1c2c2a4f21f0666a
vertical-space <chris.cowled@csiro.au>
2013-10-03 13:34:58 +1000

    another commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sys
+qual = 47	# variable (Phred score)
+
+qual_min = qual + 33
+len_min = 20
 
 infile = 'example.fastq'
 outfile = 'example_trimmed.fastq'
@@ -6,8 +9,20 @@ outfile = 'example_trimmed.fastq'
 a = open(infile, 'r').read().strip().split('@')
 z = open(outfile, 'w')
 
+discard = 0
+
 def trim(Q):
-    return len(Q)
+    global qual_min, len_min
+    out = ''
+    for chr in Q:
+        if ord(chr) < qual_min:
+            break
+        out += chr
+    if len(out) > len_min:
+        #print out
+        return len(Q)
+    else:
+        return None
 
 
 for line in a[1:]:
@@ -17,8 +32,11 @@ for line in a[1:]:
     if len(c) == 4:
         Q = c[3]
         d = trim(Q)
-        out = '\n'.join(['@'+c[0],c[1][:d+1],c[2],c[3][:d+1]])+'\n'
-        z.write(out)
+        if d != None:
+            out = '\n'.join(['@'+c[0],c[1][:d+1],c[2],c[3][:d+1]])+'\n'
+            print out,
+            z.write(out)
+        else:
+            discard += 1
 
-
-#sys.exit()
+print 'discarded reads:', discard
